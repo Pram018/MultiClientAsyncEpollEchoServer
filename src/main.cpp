a5e83b970917b34f818bb090803a7bff87ee094e
Pram018 <pramodhs316@gmail.com>
2025-11-16 12:12:02 +0530

    feat: splited server into header and .cpp and coded  thread pool for the first time

--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,16 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <iostream>
-#include <cstring>
-#define PORT 8080
+#include "include/ThreadPool.h"
+#include "include/server.h"
+#include <thread>   //std::thread::hardware_concurrency
 
-int main() 
-{
-	int server_fd, client_fd;
+int main() {
+    const int port = 8080;
 
-	struct sockaddr_in address;
-	socklen_t addrlen = sizeof(address);
-	char buffer[1024] = {0};
+    size_t numThreads = std::thread::hardware_concurrency();
+    if (numThreads == 0) numThreads = 4;
 
-	// creating a socket 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    ThreadPool pool(numThreads);
+    Server server(port, pool);
 
-	if(server_fd == -1) {
-		perror("Socket failed");
-		exit(EXIT_FAILURE);
-	}
-	
-	//setup address
-	address.sin_family =AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
-
-	// binding the socket
-	if(bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-
-	// listening
-	if(listen(server_fd, SOMAXCONN) < 0) {
-		perror("listen failed");
-		exit(EXIT_FAILURE);
-	}
-
-	std::cout << "Server listening on port " << PORT << std::endl;
-
-
-	// accepting the incoming requests
-	while(true) {
-		client_fd = accept(server_fd, (struct sockaddr*)&address, &addrlen);
-		if(client_fd < 0) {
-			perror("Accept failed");
-			continue;
-		}
-
-	//reading the data
-	int bytes_read = read(client_fd, buffer, sizeof(buffer));
-	std::cout << "Recieved:\n" << buffer << std::endl;
-
-	// Send response
-    	const char* response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
-	send(client_fd, response, strlen(response), 0);
-
-	// Close connection
-	close(client_fd);
-	}
-	return 0;
+    server.start();  // starting accepting connections
+    return 0;
 }
-
-	
-
-
-
-
-
-
-	
-
-
---
+++ src/server.cpp
@@ -0,0 +1,69 @@
+#include "include/server.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <iostream>
+#include <cstring>
+
+Server::Server(int port, ThreadPool& pool)
+    : port_(port), threadPool_(pool) {}
+
+void Server::start() {
+    server_fd_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd_ == -1) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Allow quick restart without "Address already in use"
+    int opt = 1;
+    setsockopt(server_fd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    sockaddr_in address{};
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port_);
+
+    if (bind(server_fd_, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd_, SOMAXCONN) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Server listening on port " << port_ << std::endl;
+
+    while (true) {
+        socklen_t addrlen = sizeof(address);
+        int client_fd = accept(server_fd_, (struct sockaddr*)&address, &addrlen);
+        if (client_fd < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        // Hand off client work to the thread pool
+        threadPool_.enqueue([this, client_fd]() {
+            handleClient(client_fd);
+        });
+    }
+}
+
+void Server::handleClient(int client_fd) {
+    char buffer[1024] = {0};
+    int bytes_read = read(client_fd, buffer, sizeof(buffer));
+    if (bytes_read <= 0) {
+        close(client_fd);
+        return;
+    }
+
+    std::cout << "Received:\n" << buffer << std::endl;
+
+    const char* response =
+        "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+    send(client_fd, response, strlen(response), 0);
+
+    close(client_fd);
+}
