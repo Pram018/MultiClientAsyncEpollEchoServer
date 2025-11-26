90bdc4fc4e4e8271b125e788ea244c84c2ebf22c
Pram018 <pramodhs316@gmail.com>
2025-11-27 00:40:18 +0530

    half way through the epoll implementation in the server

--- src/server.cpp
+++ src/server.cpp
@@ -8,73 +8,84 @@
 #include <string.h>
 #include <string>
 #include <fcntl.h>
-
+#include <stdexcept>
 
 
 int main() {
 
-	// Creating a socket
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if(sock == -1)
-	{
-		std::cerr << "Socket failed";
-		exit(EXIT_FAILURE);
-	}
-	
-	// Making it  a non blocking
-	int flags = fcntl(sock,F_GETFL, 0);
-	if(flags == -1) 
-	{
-		std::cerr << "fcntl(F_GETFL) failed";
-		exit(EXIT_FAILURE);
-	}
-	
-	// Declaring the port
-	const int PORT = 54000;
+    // Creating a socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
+    {
+        std::cerr << "Socket failed";
+        exit(EXIT_FAILURE);
+    }
 
-	// bind it to a port
-	struct sockaddr_in address;
-	memset(&address, 0, sizeof(address));
-	address.sin_family = AF_INET;
-	address.sin_port = htons(54000);
-	address.sin_addr.s_addr = INADDR_ANY;
+    // Making it  a non blocking
+    int flags = fcntl(sock,F_GETFL, 0);
+    if(flags == -1)
+    {
+        std::cerr << "fcntl(F_GETFL) failed";
+        exit(EXIT_FAILURE);
+    }
 
-	if(bind(sock, (struct sockaddr*)&aaddress, size0f(address)) == -1) 
-	{
-		std::cerr << "bind failed";
-		exit(EXIT_FAILURE);
-	}
+    // Declaring the port
+    const int PORT = 54000;
 
-	// mark the socket for listening in
-	if(listen(sock, SOMAXCONN) == 1)
-	{
-		std::cerr << "Can't listen";
-		exit(EXIT_FAILURE);
-	}
+    // bind it to a port
+    struct sockaddr_in address;
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(54000);
+    address.sin_addr.s_addr = INADDR_ANY;
 
-	// Creating a epool instance
-	int epoll_fd = epoll_create1(0);
-	if(epoll_fd == -1) 
-	{
-		throw std::runtime_error("Failed to connect epoll instance");
-	}
+    if(bind(sock, (struct sockaddr*)&address, sizeof(address)) == -1)
+    {
+        std::cerr << "bind failed";
+        exit(EXIT_FAILURE);
+    }
 
-	// Adding the listening sockets to epoll
-	struct epoll_event ev;
-	ev.events = EPOLLIN;
-	ev.data.fd = sock;
+    // mark the socket for listening in
+    if(listen(sock, SOMAXCONN) == -1)
+    {
+        std::cerr << "Can't listen";
+        exit(EXIT_FAILURE);
+    }
 
-	if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock, &ev) == -1)
-	{
-		throw std::runtime_error("failes to add sock to epoll");
-	}
+    // Creating a epool instance
+    int epoll_fd = epoll_create1(0);
+    if(epoll_fd == -1)
+    {
+        throw std::runtime_error("Failed to connect epoll instance");
+    }
 
+    // Adding the listening sockets to epoll
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = sock;
 
+    if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock, &ev) == -1)
+    {
+        throw std::runtime_error("failes to add sock to epoll");
+    }
 
 
+    // epoll event loop
+    const int MAX_EVENTS = 1000;
+    epoll_event events[MAX_EVENTS];
 
+    while(true) {
+        int eventCount = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
 
+        if(eventCount == -1) {
+            if(errno == EINTR) continue;
+            std::cerr << "epoll_wait failed\n";
+            break;
+        }
 
+        for(int i = 0; i < eventCount; i++)
+        {
+            int fd = events[i].data.fd;
 
 
 
